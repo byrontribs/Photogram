24eea2b25e51fa91dd73f90827c3e38ced81af00
byrontribs <byront@platform45.com>
2016-10-29 23:47:53 +0200

    second commit

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem 'rspec-rails'
   gem 'rails-controller-testing'
+  gem 'capybara'
 end
 
 group :development do
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -4,8 +4,8 @@ feature 'Creating posts' do
   scenario 'can create a post' do
     visit '/'
     click_link 'New Post'
-    attatch_file('Image', 'spec/files/images/coffie.jpg')
-    fill_in 'Caption', with: 'nom nom nom #coffietime'
+    attatch_file('Image', 'spec/files/images/coffee.jpg')
+    fill_in 'Caption', with: 'nom nom nom #coffeetime'
     click_button 'Create Post'
     expect(page).to have_content('#coffietime')
     expect(page).to have_css("img[src*='coffie.jpeg']")
